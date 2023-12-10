89658e46f1163c4c71beabe68623308d44ca0c3d
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2023-12-10 23:44:15 +0530

    basic

--- sum.java
+++ sum.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class sum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt.println("a");
-        int b = sc.nextInt.println("b");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         int sum=0;
         sum = a+b;
         System.out.println(sum);
---
+++ switch_break.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class switch_break {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int button = sc.nextInt();
+
+        switch (button) {
+            case 1:
+            System.out.println("hello");
+            break;
+        
+            case 2:
+            System.out.println("namaste");
+            break;
+
+            case 3:
+            System.out.println("kaise ho!");
+            break;
+
+            default:
+                
+        }
+    }
+}
