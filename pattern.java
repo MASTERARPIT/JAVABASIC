d95c00cfdb4373f8374386d0ff4f7f8736775bde
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2023-12-11 22:20:47 +0530

    basic

--- pattern.java
+++ pattern.java
@@ -2,6 +2,26 @@ public class pattern {
 
     public static void main(String arg[]){
 
+        // int i,n,j;
+
+        // for(i=0;i<5;i++){
+        //     for(j=0;j<5;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+        int i,j;
+
+        for(i=0;i<5;i++){
+            for(j=0;j<5;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
     }
     
 }
