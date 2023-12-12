523fe85063a224a62d7b8c50e9c56ebf15b84dc3
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2023-12-12 23:00:19 +0530

    pattern

--- pattern.java
+++ pattern.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class pattern {
 
     public static void main(String arg[]){
@@ -12,12 +14,23 @@ public class pattern {
         // }
 
 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
         int i,j;
 
-        for(i=0;i<5;i++){
-            for(j=0;j<5;j++){
-                System.out.print("* ");
+        for(i=1;i<=n;i++){
+            for(j=1;j<=m;j++){
+
+                if (i==1 || j==1 || i==n || j==m){
+                    System.out.print("* ");
+
+                }
+                else{
+                    System.out.print("  ");
+                }
+                
             }
             System.out.println();
         }
