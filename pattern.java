fc775de844567a7bad8448b4a4db7e06bce2c390
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2023-12-12 23:29:23 +0530

    pattren

--- pattern.java
+++ pattern.java
@@ -14,23 +14,68 @@ public class pattern {
         // }
 
 
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+
+
+
+
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
+
+        // int i,j;
+
+        // for(i=1;i<=n;i++){
+        //     for(j=1;j<=m;j++){
+
+        //         if (i==1 || j==1 || i==n || j==m){
+        //             System.out.print("* ");
+
+        //         }
+        //         else{
+        //             System.out.print("  ");
+        //         }
+                
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+
+        // int i,j;
+        // for (i=0;i<5;i++){
+        //     for(j=0;j<i;j++){
+                
+        //         System.out.print("* ");
+
+        //     }
+        //     System.out.println();
+        // }
+        
+        
+        
+        
+        // int i,j;
+        // for (i=5;i>0;i--){
+        //     for(j=0;j<i;j++){
+                
+        //         System.out.print("* ");
+
+        //     }
+        //     System.out.println();
+        // }
+
+        
 
         int i,j;
+        for(i=0;i<=5;i++){
+            for(j=0;j<=5-i;j++){
+                System.out.print("  ");
 
-        for(i=1;i<=n;i++){
-            for(j=1;j<=m;j++){
+            }
 
-                if (i==1 || j==1 || i==n || j==m){
-                    System.out.print("* ");
-
-                }
-                else{
-                    System.out.print("  ");
-                }
-                
+            for(j=0;j<=i;j++){
+                System.out.print("* ");
             }
             System.out.println();
         }
