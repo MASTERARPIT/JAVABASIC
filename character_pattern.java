01549e156863410488e20470f38fb907304eb545
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2023-12-28 20:03:08 +0530

    basic

---
+++ character_pattern.java
@@ -0,0 +1,19 @@
+public class character_pattern {
+
+    public static void main(String[] args) {
+
+        char ch ='A';
+        int i,j,n=6;
+
+        for(i=1;i<=n;i++){
+            for(j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println(" ");
+            
+        }
+        
+    }
+    
+}
--- pattern.java
+++ pattern.java
@@ -67,19 +67,39 @@ public class pattern {
 
         
 
-        int i,j;
-        for(i=0;i<=5;i++){
-            for(j=0;j<=5-i;j++){
-                System.out.print("  ");
+        // int i,j;
+        // for(i=0;i<=5;i++){
+        //     for(j=0;j<=5-i;j++){
+        //         System.out.print("  ");
 
-            }
+        //     }
 
-            for(j=0;j<=i;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
+        //     for(j=0;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        
+
+
 
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
