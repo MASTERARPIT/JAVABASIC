2ebcc69f040331a3e1e646e4d2fef89efedf3709
ARPIT KUMAR <131479233+MASTERARPIT@users.noreply.github.com>
2024-01-03 23:10:38 +0530

    function overload

--- Function_overloading.java
+++ Function_overloading.java
@@ -1,15 +1,19 @@
 public class Function_overloading {
 
-    public static int sum(int a,int b){
+    public static int sum(int a,int b){ //functin overloading using parameters
         return a+b;
     }
     public static int sum(int a,int b,int c){
         return a+b+c;
     }
+    public static float sum(float a,float b){ //function overloading using different datatypes
+        return a+b;
+    }
     public static void main(String[] args){
 
         System.out.println(sum(3,5));
         System.out.println(sum(3,5,8));
+        System.out.println(sum(3.2f,4.8f));
 
     }
 }
